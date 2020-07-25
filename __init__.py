0920a1c5e5ee1c10f2c9841b0efa5212ecde4c05
Jose Motta <josemotta@bampli.com>
2020-07-25 11:00:10 -0300

    add root to sys.path

--- __init__.py
+++ __init__.py
@@ -1,1 +1,8 @@
 print("arrived to main module")
+
+import os, sys
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__)) #  root dir
+ROOT_DIR = os.path.dirname(THIS_DIR) #  points to root dir
+if ROOT_DIR not in sys.path:
+    sys.path.insert(0, ROOT_DIR)
